7d59304375be1b312bfea5265817d6177fd6c0e0
unknown <swayamyadav13@gmail.com>
2020-10-30 17:44:06 +0530

    slingshot

--- sketch.js
+++ sketch.js
@@ -38,8 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    
+    slingShot = new slingshot(bird.body,{x:200,y:140});
 }
 
 function draw(){
@@ -63,6 +63,17 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    
+    slingShot.display();    
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+
+
+
+}
+
+function mouseReleased(){
+    slingShot.fly();
 }
